08e8702ba1386dcea63eb309a5f378ebe67ac9e0
Amirul <rafikhan97ctg@gmail.com>
2022-11-04 16:13:44 +0600

    barcode

---
+++ products/bcScn.php
@@ -0,0 +1,68 @@
+<?php 
+	include("../server/connection.php");
+	include '../set.php';
+	$sql = "SELECT * FROM products";
+	$result	= mysqli_query($db, $sql);
+	$deleted = isset($_GET['deleted']);
+	$added  = isset($_GET['added']);
+	$updated = isset($_GET['updated']);
+	$undelete = isset($_GET['undelete']);
+	$error = isset($_GET['error']);
+	$failure = "";	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('../templates/head1.php');?>
+</head>
+<body>
+	<div class="contain h-100">
+		<?php include('../products/base.php');?>
+		<div>
+			<h1 class="ml-4 pt-2"><i class="fas fa-barcode"></i> Barcode Scanner</h1>
+			<hr>
+			<?php include('../alert.php');?>
+			<div class="table-responsive mt-4 pl-5 pr-5">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iure ipsam, necessitatibus officia assumenda ad odit? Quae cumque accusantium, facilis similique et veniam dolore?</p>
+			<div class="mt-1 ">
+			<div class="input-group"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span></div>
+   				<input class="form-control" type="text" placeholder="Product Search" aria-label="Search" id="search" name="search" onkeyup="loadproducts();"/>
+   			</div></div>
+			   <div id="product_area" style="padding:5px;" class="table-responsive-sm mt-2 table-wrapper-scroll-y my-custom-scrollbar" >
+				<table class="w-100 table-striped font-weight-bold" style="cursor: pointer;" id="table1">
+					<thead>
+						<tr claclass='text-center'><b>
+							<td>Barcode</td>
+							<td>Product Name</td>
+							<td>Price</td>
+							<td>Unit</td>
+							<td>Stocks</td>
+							<td>Location</td>
+						</tr></b>
+						<tbody id="products">
+							
+						</tbody>
+					</thead>
+				</table>
+			</div>
+			</div>
+		</div>
+	</div>
+	<script src="../bootstrap4/jquery/jquery.min.js"></script>
+	<script src="../bootstrap4/js/jquery.dataTables.js"></script>
+	<script src="../bootstrap4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="../bootstrap4/js/bootstrap.bundle.min.js"></script>
+	<?php include('../products/delete_products.php');?>
+</body>
+</html>
+<div id="dataModal" class="modal fade bd-example-modal-md" data-backdrop="static" data-keyboard="false">  
+	<div class="modal-dialog modal-md"  role="document">  
+		<div class="modal-content">   
+		<div class="modal-body d-inline" id="Contact_Details"></div> 
+			<div class="modal-footer"> 
+				<input type="button" class="btn btn-default btn-success" data-dismiss="modal" value="Okay">   
+			</div>  
+	   </div>  
+	</div>  
+</div>
+<script src="../products/javascript.js"></script>
